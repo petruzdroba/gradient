d1f22b7ae91948a3d29e2127dc7de646d4989163
Petru Zdroba <petru.zdroba@gmail.com>
2026-02-16 13:43:57 +0200

    v3.1

--- gradient/src/main/resources/static/script.js
+++ gradient/src/main/resources/static/script.js
@@ -3,10 +3,20 @@ function generateGradient() {
     img.src = '/generate-gradient?' + new Date().getTime();
 }
 
-document.getElementById('generateBtn').addEventListener('click', generateGradient);
+const button = document.getElementById('generate-btn');
+
+button.addEventListener('click', generateGradient);
 document.getElementById('gradient').addEventListener('click', generateGradient);
 
+button.classList.add('hidden');
+githubLink.classList.add('hidden');
+
 document.addEventListener('keydown', function(event) {
+    if (event.key === 'b' || event.key === 'B') {
+        button.classList.toggle('hidden');
+        githubLink.classList.toggle('hidden');
+    }
+
     if (event.code === 'Space') {
         event.preventDefault();
         generateGradient();
